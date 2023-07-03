06628bb6169d1ec9f7d96188324ada9c59d56fc0
Lucas Climenhage <luke.climenhage@gmail.com>
2023-07-03 14:18:34 -0400

    Change tile base tag from div to button

--- src/components/KeyBoardTile.vue
+++ src/components/KeyBoardTile.vue
@@ -1,7 +1,7 @@
 <template>
-  <div
+  <button
     @click="$emit('keyClicked', content)"
-    class="h-15 rounded-md overflow-hidden opacity-100 active:brightness-75 transition-opacity"
+    class="h-15 rounded-md overflow-hidden opacity-100 active:brightness-75 transition-opacity margin-auto"
     :class="content === 'back' || content === 'enter' ? 'max-w-16' : 'max-w-11'"
   >
     <LetterBackground :letter-state="letterState">
@@ -21,7 +21,7 @@
         />
       </template>
     </LetterBackground>
-  </div>
+  </button>
 </template>
 
 <script setup>
