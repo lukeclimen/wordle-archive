0044857ebf5f741bc17009351f0c142899a61909
Lucas Climenhage <luke.climenhage@gmail.com>
2023-07-03 15:21:50 -0400

    Add props for guesses to KeyBoard

--- src/stories/KeyBoard.stories.js
+++ src/stories/KeyBoard.stories.js
@@ -5,9 +5,36 @@ export default {
   component: KeyBoard,
   parameters: {
     layout: 'fullscreen'
+  },
+  argTypes: {
+    correctLetters: {
+      control: 'disabled'
+    },
+    wrongPositionLetters: {
+      control: 'disabled'
+    },
+    wrongGuessLetters: {
+      control: 'disabled'
+    }
   }
 }
 
-export const Blank = {
+export const Default = {
   args: {}
+}
+
+export const SomeGuesses = {
+  args: {
+    correctLetters: ['a', 'y'],
+    wrongPositionLetters: ['t'],
+    wrongGuessLetters: ['q', 'g', 'm']
+  }
+}
+
+export const MostGuessed = {
+  args: {
+    correctLetters: ['a', 'y', 's'],
+    wrongPositionLetters: ['t', 'r'],
+    wrongGuessLetters: ['q', 'g', 'm', 'o', 'l', 'c', 'v', 'p']
+  }
 }
