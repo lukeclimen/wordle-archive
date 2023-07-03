9d3683b134330b2f8626085ba4dd1a218c34c5f9
Lucas Climenhage <luke.climenhage@gmail.com>
2023-07-03 14:30:41 -0400

    Create KeyBoard component and story

---
+++ src/components/KeyBoard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex flex-col w-screen gap-y-2 p-2">
+    <ul id="top-row" class="flex flex-row w-full gap-x-1.5 justify-center">
+      <li v-for="(letter, index) in topRowKeys" :key="index" class="flex-grow">
+        <KeyBoardTile :content="letter" @keyClicked="handleKeyClick" class="w-full" />
+      </li>
+    </ul>
+    <ul id="middle-row" class="flex flex-row gap-x-1.5 justify-center">
+      <li v-for="(letter, index) in middleRowKeys" :key="index" class="flex-grow">
+        <KeyBoardTile :content="letter" @keyClicked="handleKeyClick" class="w-full" />
+      </li>
+    </ul>
+    <ul id="bottom-row" class="flex flex-row gap-x-1.5 justify-center">
+      <li v-for="(letter, index) in bottomRowKeys" :key="index" class="flex-grow">
+        <KeyBoardTile :content="letter" @keyClicked="handleKeyClick" class="w-full" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import KeyBoardTile from './KeyBoardTile.vue'
+
+const topRowKeys = ref(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'])
+const middleRowKeys = ref(['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'])
+const bottomRowKeys = ref(['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'back'])
+
+const handleKeyClick = (content) => console.log(content)
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/stories/KeyBoard.stories.js
@@ -0,0 +1,13 @@
+import KeyBoard from '../components/KeyBoard.vue'
+
+export default {
+  title: 'KeyBoard',
+  component: KeyBoard,
+  parameters: {
+    layout: 'fullscreen'
+  }
+}
+
+export const Blank = {
+  args: {}
+}
