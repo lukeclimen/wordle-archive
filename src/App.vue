5b541ac62991893b06f115a0fa40bc299516228e
Lucas Climenhage <Lucas.Climenhage@rwdi.com>
2023-07-13 21:19:31 -0400

    Remove LetterGrid component, place in App.vue
    Instead of having an extra layer of component, I've decided to assemble
    the grid on the page and cut out the middleman

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <h1 class="bg-blue-500 text-xl font-bold text-white mb-10">Hello World</h1>
-  <HelloWorld msg="Hey there" />
+  <div class="h-screen max-w-md mx-auto py-3 flex flex-col justify-center">
+    <div class="flex flex-col gap-y-2 pb-3">
+        <GridRow v-for="(element, index) in gridRows" :key="index"/>
+    </div>
+    <KeyBoard />
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+import GridRow from './components/GridRow.vue';
+import KeyBoard from './components/KeyBoard.vue';
+const gridRows = ref([0, 1, 2, 3, 4, 5]);
 </script>
