e5b61e6e27d32439eca6304ef4600a633d35e961
Lucas Climenhage <luke.climenhage@gmail.com>
2024-01-06 17:21:31 -0500

    Create basic SiteHeader component

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="flex flex-row items-center justify-around w-full h-20 border-b-2 border-gray-700 shadow-lg"
+  >
+    <LogoIcon />
+    <GearIcon />
+  </div>
+</template>
+
+<script setup>
+import LogoIcon from '../assets/LogoIcon.vue';
+import GearIcon from '../assets/GearIcon.vue';
+</script>
+
+<style scoped></style>
