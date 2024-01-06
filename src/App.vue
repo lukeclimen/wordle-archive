b4dee360091299b76c6ad80e12750d69bc903902
Lucas Climenhage <luke.climenhage@gmail.com>
2024-01-05 22:09:30 -0500

    Accomodate short/wide combo screens

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="h-screen max-w-lg mx-auto py-3 flex flex-col justify-center">
+  <div class="h-screen md:max-w-md lg:max-w-lg mx-auto py-3 flex flex-col justify-center">
     <div class="flex flex-col gap-y-2 pb-3">
       <GridRow
         v-for="(element, index) in gridRowContents"
+        :class="shortWideScreen ? 'max-w-xs' : 'max-w-md'"
         :key="index"
         :locked="index < activeRow ? true : false"
         :guess="index === activeRow ? currentRowGuess : element"
@@ -18,7 +19,7 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useGameStore } from './stores/GameStore';
 import GridRow from './components/GridRow.vue';
 import KeyBoard from './components/KeyBoard.vue';
@@ -30,6 +31,7 @@ const { getCorrectLetterArray, getwrongGuessLetterArray, getwrongPositionLetterA
 const gridRowContents = computed(() => gameStore.getGuessList);
 const activeRow = computed(() => gameStore.getGuessCount);
 const currentRowGuess = computed(() => gameStore.getCurrentGuessWord);
+const shortWideScreen = ref(false);
 
 const handleLetterPress = (content) => {
   if (content === 'enter') {
@@ -43,5 +45,11 @@ const handleLetterPress = (content) => {
 
 onMounted(() => {
   gameStore.fetchWordOfTheDay();
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+
+  if (screenHeight < 2 * screenWidth) {
+    shortWideScreen.value = true;
+  }
 });
 </script>
