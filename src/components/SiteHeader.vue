913728d81a53f808b3c3863e15bd548baff5de9c
Lucas Climenhage <luke.climenhage@gmail.com>
2024-01-06 21:57:05 -0500

    Add new gear icon to header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -3,13 +3,13 @@
     class="flex flex-row items-center justify-around w-full h-20 border-b-2 border-gray-700 shadow-lg"
   >
     <LogoIcon />
-    <GearIcon />
+    <Cog6ToothIcon class="cursor-pointer w-10 aspect-square" />
   </div>
 </template>
 
 <script setup>
 import LogoIcon from '../assets/LogoIcon.vue';
-import GearIcon from '../assets/GearIcon.vue';
+import { Cog6ToothIcon } from '@heroicons/vue/24/solid';
 </script>
 
 <style scoped></style>
