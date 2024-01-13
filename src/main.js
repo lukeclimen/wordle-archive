e500bed6c1612c3d58d2039b9a549c2318c9efb4
Lucas Climenhage <luke.climenhage@gmail.com>
2024-01-13 11:22:10 -0500

    Limit number of toasts at one point in time

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import Vue3Toasity from 'vue3-toastify';
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(Vue3Toasity, {
+  limit: 4
+});
 
 app.mount('#app');
