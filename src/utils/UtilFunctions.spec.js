1eb874a9fedb704302a12cf5827caceff0837670
Lucas Climenhage <luke.climenhage@gmail.com>
2024-01-13 14:28:01 -0500

    Add new function for searching guess list

---
+++ src/utils/UtilFunctions.spec.js
@@ -0,0 +1,65 @@
+import { describe, expect, it } from 'vitest';
+import { searchForAcceptableGuess } from './UtilFunctions';
+
+describe('Guess Array Search function', () => {
+  it('Finds words that exist in the array', () => {
+    const existingWords = [
+      'aglow',
+      'cigar',
+      'above',
+      'weary',
+      'shore',
+      'cloud',
+      'about',
+      'query',
+      'sound',
+      'crane'
+    ];
+
+    existingWords.forEach((word) => {
+      const result = searchForAcceptableGuess(word);
+      expect(result).toStrictEqual(true);
+    });
+  });
+
+  it("Rejects words that don't exist in the array", () => {
+    const nonExistantWords = [
+      'archive',
+      'winner',
+      'champion',
+      'testing',
+      'friend',
+      'bicycle',
+      'jackson',
+      'volley',
+      'quaker',
+      'willow'
+    ];
+
+    nonExistantWords.forEach((word) => {
+      const result = searchForAcceptableGuess(word);
+      expect(result).toStrictEqual(false);
+    });
+  });
+
+  it('Finds words at the edge of the array', () => {
+    const edgeWords = [
+      'aahed',
+      'aalii',
+      'aargh',
+      'aarti',
+      'abaca',
+      'zurfs',
+      'zuzim',
+      'zygal',
+      'zygon',
+      'zymes',
+      'zymic'
+    ];
+
+    edgeWords.forEach((word) => {
+      const result = searchForAcceptableGuess(word);
+      expect(result).toStrictEqual(true);
+    });
+  });
+});
