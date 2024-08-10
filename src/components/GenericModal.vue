3c37b2677b7613a1bc63100ee8ea29691beab2a3
Lucas Climenhage <luke.climenhage@gmail.com>
2024-08-10 14:15:23 -0400

    Create generic modal template

---
+++ src/components/GenericModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="z-50 w-screen h-screen fixed top-0 left-0 smoke">
+    <div
+      class="flex flex-col w-5/6 lg:max-w-md h-2/3 fixed top-0 bottom-0 left-0 right-0 mx-auto my-auto bg-white rounded-2xl shadow-2xl"
+    >
+      <div class="w-full border-b-2 border-gray-500 text-xl uppercase text-center py-4">
+        <h2>{{ modalTitle }}</h2>
+        <XMarkIcon
+          class="w-8 aspect-square absolute top-0 bottom-0 my-auto right-4 text-gray-800 cursor-pointer"
+          @click="$emit('close')"
+        />
+      </div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { XMarkIcon } from '@heroicons/vue/24/solid';
+defineProps({
+  modalTitle: {
+    type: String,
+    required: true
+  }
+});
+defineEmits(['close']);
+</script>
+
+<style scoped></style>
