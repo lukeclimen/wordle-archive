597f33517da9f350b2cb40937b8ff712c3d93ba4
Lucas Climenhage <luke.climenhage@gmail.com>
2024-08-10 12:14:55 -0400

    Add calendar button to site header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -3,13 +3,17 @@
     class="flex flex-row items-center justify-around w-full h-20 border-b-2 border-gray-700 shadow-lg"
   >
     <LogoIcon :isMobile="isMobile" />
-    <IconButton :icon="Cog6ToothIcon" @click="$emit('openSettings')" />
+    <div class="flex flex-row gap-x-4">
+      <IconButton :icon="CalendarDaysIcon" @click="$emit('openCalendar')" />
+      <IconButton :icon="Cog6ToothIcon" @click="$emit('openSettings')" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import LogoIcon from '../assets/LogoIcon.vue';
 import { Cog6ToothIcon } from '@heroicons/vue/24/solid';
+import { CalendarDaysIcon } from '@heroicons/vue/24/outline';
 import IconButton from './IconButton.vue';
 defineProps({
   isMobile: {
@@ -17,7 +21,7 @@ defineProps({
     default: true
   }
 });
-defineEmits('openSettings');
+defineEmits(['openSettings', 'openCalendar']);
 </script>
 
 <style scoped></style>
