71ea8a28935d804e7c1a9b2b67108555673370b6
Lucas Climenhage <luke.climenhage@gmail.com>
2024-08-10 16:17:04 -0400

    Add collapse prop to generic modal template

--- src/components/modals/CalendarModal.vue
+++ src/components/modals/CalendarModal.vue
@@ -1,11 +1,21 @@
 <template>
-  <GenericModal modalTitle="Calendar" @close="$emit('close')"></GenericModal>
+  <GenericModal modalTitle="Calendar" @close="$emit('close')">
+    <DatePicker v-model="date" mode="date" />
+  </GenericModal>
 </template>
 
 <script setup>
 import GenericModal from './GenericModal.vue';
+import { DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
+import { ref, watch } from 'vue';
 
 defineEmits(['close']);
+
+const date = ref(new Date());
+watch(date, async () => {
+  console.log(date.value);
+});
 </script>
 
 <style scoped></style>
