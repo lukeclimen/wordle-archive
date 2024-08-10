45b4116328304e8c508c1959229887e47222a2b6
Lucas Climenhage <luke.climenhage@gmail.com>
2024-08-10 12:01:26 -0400

    Replace settings icon with button

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -3,16 +3,14 @@
     class="flex flex-row items-center justify-around w-full h-20 border-b-2 border-gray-700 shadow-lg"
   >
     <LogoIcon :isMobile="isMobile" />
-    <Cog6ToothIcon
-      class="cursor-pointer w-10 aspect-square"
-      @click="$emit('openSettings')"
-    />
+    <IconButton :icon="Cog6ToothIcon" @click="$emit('openSettings')" />
   </div>
 </template>
 
 <script setup>
 import LogoIcon from '../assets/LogoIcon.vue';
 import { Cog6ToothIcon } from '@heroicons/vue/24/solid';
+import IconButton from './IconButton.vue';
 defineProps({
   isMobile: {
     type: Boolean,
