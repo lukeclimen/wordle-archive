2e9d26aaf2648cf20f77c907fda297364b577edd
Lucas Climenhage <luke.climenhage@gmail.com>
2024-08-10 12:00:43 -0400

    Make button background lighter

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex justify-center w-10 h-10 bg-gray-100 border border-gray-200 rounded shadow-lg active:translate-y-0.5"
+    class="flex justify-center w-10 h-10 bg-gray-50 border border-gray-200 rounded shadow-lg active:translate-y-0.5"
   >
     <component :is="icon" class="h-8 w-8 text-neutral-800 self-center"></component>
   </button>
