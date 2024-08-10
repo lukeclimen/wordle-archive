7da46e4a8c55f6fed0c1ba13bd3d4e6160420485
Lucas Climenhage <luke.climenhage@gmail.com>
2024-08-10 16:25:55 -0400

    Add width to the collapse properties

--- src/components/modals/GenericModal.vue
+++ src/components/modals/GenericModal.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="z-50 w-screen h-screen fixed top-0 left-0 smoke">
     <div
-      class="flex flex-col w-5/6 lg:max-w-md h-2/3 fixed top-0 bottom-0 left-0 right-0 mx-auto my-auto bg-white rounded-2xl shadow-2xl"
+      class="flex flex-col lg:max-w-md fixed top-0 bottom-0 left-0 right-0 mx-auto my-auto bg-white rounded-2xl shadow-2xl"
+      :class="[collapse ? 'w-fit h-fit' : 'w-5/6 h-2/3']"
     >
       <div class="w-full border-b-2 border-gray-500 text-xl uppercase text-center py-4">
         <h2>{{ modalTitle }}</h2>
@@ -21,6 +22,11 @@ defineProps({
   modalTitle: {
     type: String,
     required: true
+  },
+  collapse: {
+    type: Boolean,
+    required: false,
+    default: false
   }
 });
 defineEmits(['close']);
